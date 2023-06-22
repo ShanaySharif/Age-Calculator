2911335cc2c8c8d3d28c618d4a894cd38b6d73c4
ShanaySharif <Shanay.uw17@gmail.com>
2023-06-22 13:09:05 -0400

    1st passing test that calculates Mercury years

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -1,12 +1,18 @@
-describe('calculateMercuryAge', () => {
+describe('calculateAge', () => {
     class Calculator {
         constructor(number) {
           this.number = number;
         }
+        calculateMercuryAge() {
+            return Math.round(this.number / 0.24);
+        }
       }
+
       test('it should take a person\'s age in years and return Mercury years', () => {
         const calculator = new Calculator(30);
         const mercuryAge = calculator.calculateMercuryAge();
         expect(mercuryAge).toBe(125);
       });
     });
+
+    
