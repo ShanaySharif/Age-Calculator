6eb6e6d904e5195433915f02445a0774e12ae00b
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-02 19:44:15 -0400

    moving class/functions in js file apart from test

--- __tests__/age-calculator.test.js
+++ __tests__/age-calculator.test.js
@@ -1,54 +1,5 @@
-class Calculator {
-  constructor(number) {
-    this.number = number;
-  }
-  calculateMercuryAge() {
-    const mercuryAge = this.number / 0.24;
-    return Math.round(mercuryAge * 100) / 100;
-  }
-  calculateVenusAge() {
-    const venusAge = this.number / 0.62;
-    return Math.round(venusAge * 100) / 100;
-  }
-  calculateMarsAge() {
-    const marsAge = this.number / 1.88;
-    return Math.round(marsAge * 100) / 100;
-  }
-  calculateJupiterAge() {
-    const jupiterAge = this.number / 11.86;
-    return Math.round(jupiterAge * 100) / 100;
-  }
-  calculateYearsPassed(age, pastBirthYear) {
-    const earthYearsPassed = age - pastBirthYear;
-    const yearsPassed = new Calculator(earthYearsPassed);
-    const marsYearsPassed = yearsPassed.calculateMarsAge();
-    const mercuryYearsPassed = yearsPassed.calculateMercuryAge();
-    const venusYearsPassed = yearsPassed.calculateVenusAge();
-    const jupiterYearsPassed = yearsPassed.calculateJupiterAge();
-    return {
-      earthYearsPassed,
-      marsYearsPassed,
-      mercuryYearsPassed,
-      venusYearsPassed,
-      jupiterYearsPassed,
-    };
-  }
-  calculateYearsYetToPass(age, futureAge) {
-    const earthYearsYetToPass = futureAge - age;
-    const years = new Calculator(earthYearsYetToPass);
-    const marsYearsYetToPass = years.calculateMarsAge();
-    const mercuryYearsYetToPass = years.calculateMercuryAge();
-    const venusYearsYetToPass = years.calculateVenusAge();
-    const jupiterYearsYetToPass = years.calculateJupiterAge();
-    return {
-      earthYearsYetToPass,
-      marsYearsYetToPass,
-      mercuryYearsYetToPass,
-      venusYearsYetToPass,
-      jupiterYearsYetToPass,
-    };
-  }
-}
+import { Calculator } from "../src/age-calculator";
+//UI Logic
 describe("calculateAge", () => {
   test("it should take a person's age in years and return Mercury years", () => {
     const calculator = new Calculator(30);
